ddb422e3e6d32599f6f6be4ec35e4044090ecf74
Lomar Berry <lxberry@gmail.com>
2013-03-30 19:39:51 -0400

    Updates to tbalden CM gerrit suggestions

--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
@@ -129,9 +129,4 @@ public class BridgePowerManager implements IPowerManager {
     public void setKeyboardVisibility(boolean visible) {
         
     }
-    
-    @Override
-    public void setKeyboardLight(boolean on, int key) {
-        // pass for now
-    }
 }
